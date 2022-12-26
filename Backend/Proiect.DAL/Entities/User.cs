fe5f6ef3124f9364d4b2f518d3254cc99fcb59e2
Vlad Andries <vlad.andries@code11.com>
2022-12-26 20:33:56 +0200

    add the untracked files too

---
+++ Backend/Proiect.DAL/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Proiect.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect.DAL.Entities
+{
+    public class User : IdentityUser<int>
+    {
+        public string RefreshToken { get; set; }
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+    }
+}
---
+++ Backend/Proiect/Service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proiect
+{
+    public class Service
+    {
+        public string ReturnString(int i)
+        {
+            return $"Inpus was {i}";
+        }
+    }
+}
