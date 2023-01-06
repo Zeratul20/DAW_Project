b1a6fa6caacdb95837b314bd21bf0674c075fada
Vlad Andries <vlad.andries@code11.com>
2023-01-06 07:42:02 +0200

    fix last commit

---
+++ Backend/Proiect.DAL/Migrations/20230105112629_AddedDesignerAddressTable.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proiect.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDesignerAddressTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DesignerAddresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Zipcode = table.Column<int>(type: "int", nullable: false),
+                    DesignerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DesignerAddresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DesignerAddresses_Designers_DesignerId",
+                        column: x => x.DesignerId,
+                        principalTable: "Designers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DesignerAddresses_DesignerId",
+                table: "DesignerAddresses",
+                column: "DesignerId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DesignerAddresses");
+        }
+    }
+}
