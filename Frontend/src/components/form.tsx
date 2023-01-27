bf91ece11f55be418c8a5e5586ce2be7583ebf3f
Vlad Andries <vlad.andries@code11.com>
2023-01-27 13:21:00 +0200

    add Add designer button + form, delete 2 files that are no longer used

---
+++ Frontend/src/components/form.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+export const Form: view = ({ data }) => {
+  return (
+    <div>
+      <h3>Add a designer</h3>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        {Object.keys(data).map((d) => {
+          console.log(d);
+          if (d !== "address")
+            return (
+              <TextField key={d} id="input_key" label={d} variant="standard" />
+            );
+          const address = data[d];
+          return Object.keys(address).map((a) => (
+            <TextField
+              key={a}
+              id="input_key"
+              label={"address " + a}
+              variant="standard"
+            />
+          ));
+        })}
+      </Box>
+      <Stack spacing={2} direction="row"></Stack>
+      <Button variant="outlined">Add</Button>
+    </div>
+  );
+};
--- Frontend/src/views/designers.tsx
+++ Frontend/src/views/designers.tsx
@@ -6,44 +6,56 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Button } from "@mui/material";
 
+import { Form } from "../components/form";
 import { designers } from "../mock-data/designers";
 
 export const Designers: view = () => {
   if (!designers) {
     return null;
   }
-  let cnt: Number = 1;
+  let firstDesigner = {};
+  for (let designer in designers) {
+    firstDesigner = designers[designer];
+    break;
+  }
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Designers</TableCell>
-            <TableCell align="right">Age&nbsp;</TableCell>
-            <TableCell align="right">Gender&nbsp;</TableCell>
-            <TableCell align="right">Address&nbsp;</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Object.keys(designers).map((designer) => (
-            <TableRow
-              key={designers[designer].id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {designers[designer].name}
-              </TableCell>
-              <TableCell align="right">{designers[designer].age}</TableCell>
-              <TableCell align="right">{designers[designer].gender}</TableCell>
-              <TableCell align="right">
-                {designers[designer].address.city} |{" "}
-                {designers[designer].address.zipcode}
-              </TableCell>
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Designers</TableCell>
+              <TableCell align="right">Age&nbsp;</TableCell>
+              <TableCell align="right">Gender&nbsp;</TableCell>
+              <TableCell align="right">Address&nbsp;</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {Object.keys(designers).map((designer) => (
+              <TableRow
+                key={designers[designer].id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {designers[designer].name}
+                </TableCell>
+                <TableCell align="right">{designers[designer].age}</TableCell>
+                <TableCell align="right">
+                  {designers[designer].gender}
+                </TableCell>
+                <TableCell align="right">
+                  {designers[designer].address.city} |{" "}
+                  {designers[designer].address.zipcode}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Form data={firstDesigner} key={firstDesigner}/>
+      {/* <Button></Button> */}
+    </div>
   );
 };
